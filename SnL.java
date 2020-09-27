23309f20985a2a08f2fb0d7ed35e38f2f34fd7c3
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-27 22:37:39 +0530

    UC3

--- SnL.java
+++ SnL.java
@@ -5,8 +5,24 @@ public class SnL {
 	{
 		int position =0;
 		int countOfDieRoll=0;
+		final int noplay=0;
+		final int ladder=1;
+		final int snake=2;
 		int die = (int) Math.floor(Math.random()*6)+1;
-		
+		int option= (int) Math.floor(Math.random()*3);
+		switch(option)
+		{
+		case ladder:
+			position=position+die;
+			break;
+		case snake:
+			position=position-die;
+			break;
+		case noplay:
+			position=position;
+			break;
+			
+		}
 	}
 
 }
