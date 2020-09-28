3750c471d120924bff85b71a2a044952ad599e26
Sakshat Bagadi <bagadisakshat@gmail.com>
2020-09-28 11:49:05 +0530

    UC7

--- SnL.java
+++ SnL.java
@@ -1,48 +1,68 @@
 package snakenladder;
-
 public class SnL {
-	public static void main(String[] args)
-	{
-		int position =0;
-		int countOfDieRoll=0;
-		final int noplay=0;
-		final int ladder=1;
-		final int snake=2;
-		do
+	    static boolean turndecider=true;
+	    public static int playDice (int position,String Player)
+	    {
+	    	   final int NO_PLAY=0;
+		       final int LADDER=1;
+		       final int SNAKE=2;  
+		       
+	    	       int  die = (int) ( Math.floor(Math.random()*6) + 1);
+	    	       int option=(int) Math.floor(Math.random()*3);
+                       switch(option) {
+                
+                          case NO_PLAY:
+                          break;
+                
+                          case LADDER:
+                          position=position + die;
+                          if(position>100)
+                          {
+                             position=position - die;
+                          }
+                          playDice (position,Player);
+                          break;
+                
+                          case SNAKE:
+                          position=position - die;
+                          if(position<0)
+                          {
+                             position=0;
+                          }
+                          break;
+                       }
+                       System.out.println("Position of "+ Player +" "+ position);
+                       return position;
+	    }
+
+            public static void main(String[] args)
 		{
-			int die = (int) Math.floor(Math.random()*6)+1;
-			int option= (int) Math.floor(Math.random()*3);
-			switch(option)
-			{
-			case ladder:
-				position=position+die;
-				break;
-			case snake:
-				position=position-die;
-				break;
-			case noplay:
-				position=position;
-				break;
-				
-			}
-			if(position < 0)
-			{
-				position=0;
-			}
-			if(position > 100)
-			{
-				position=position-die;
-			}
-			
-			countOfDieRoll++;
-			System.out.println("Position of Player: "+ position);
-			
-		}while(position!=100);
-		
-		System.out.println("number of die rolls to win: "+ countOfDieRoll);
-		
-		
-	}
+		          
+		       int positionOfP1=0;
+		       int positionOfP2=0;
+		       while(positionOfP1<100 && positionOfP2<100)
+		       {
+
+                       if(turndecider)
+                       {
+            	          positionOfP1=playDice(positionOfP1, "Player1");
+                       }
+                       else
+                       {
+            	          positionOfP2=playDice(positionOfP2, "Player2");
+                       }
+		           turndecider=!turndecider;
+		       }
+		       if(positionOfP1==100)
+		       {
+		    	   System.out.println("Player1 wins");
+		       }
+		       else
+		       {
+		    	   System.out.println("Player2 wins");
+		       }
+		}
+		        
 
 }
 
